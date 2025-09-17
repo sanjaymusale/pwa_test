acc242e7f494b246376f42a323546698d59fcf79
SanjayTheCoder <sanjay@reckonsys.com>
2025-09-17 17:19:53 +0530

    new update

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>my-pwa-app new</h1>
+      <h1>my-pwa-app UPDATED</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
