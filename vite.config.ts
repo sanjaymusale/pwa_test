551355893f766face4933df4c9da172c482fe516
SanjayTheCoder <sanjay@reckonsys.com>
2025-09-17 17:21:44 +0530

    auto update

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), VitePWA({
-    registerType: 'prompt',
+    registerType: 'autoUpdate',
     injectRegister: false,
 
     pwaAssets: {
