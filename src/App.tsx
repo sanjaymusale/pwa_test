4263cb18308e392045a1b71cff163d41f771926c
SanjayTheCoder <sanjay@reckonsys.com>
2025-09-17 17:33:24 +0530

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>my-pwa-app auto</h1>
+      <h1>TESTING</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
